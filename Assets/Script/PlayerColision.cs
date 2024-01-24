8de23d34fb047e2bc1e78f50b8b7c5187ee0cbf3
Eun6636 <tktmdmswl004@gmail.com>
2024-01-24 20:06:36 +0900

    feat - 캐릭터 충돌

---
+++ Assets/Script/PlayerColision.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerColision : MonoBehaviour
+{
+
+    //장애물에 닿을때
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+    }
+
+
+}
