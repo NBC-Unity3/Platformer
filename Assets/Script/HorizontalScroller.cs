52f6f583c8305c7535d6eac45131456bf50dedae
Siryu <siryu2409@gmail.com>
2024-01-26 15:47:02 +0900

    perf : set HorizontalScrolller to support various size
    replace magic number to pre calculated width

--- Assets/Script/HorizontalScroller.cs
+++ Assets/Script/HorizontalScroller.cs
@@ -3,16 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Animations;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class HorizontalScroller : MonoBehaviour
 {
     [SerializeField]
     float scrollSpeed = 1.0f;
+
+    [SerializeField]
+    SpriteRenderer spriteRenderer;
+
     Vector2 moveDirection;
+    float width = 0;
 
     private void Awake()
     {
         InputController controller = GameObject.FindWithTag("Player").GetComponent<InputController>();
         controller.OnMoveEvent += SetMoveDirection;
+
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+
+        width = ((float)spriteRenderer.sprite.textureRect.width * transform.localScale.x) / spriteRenderer.sprite.pixelsPerUnit;
     }
 
     private void SetMoveDirection(Vector2 direction)
@@ -30,14 +40,14 @@ public class HorizontalScroller : MonoBehaviour
         ApplyMovement(moveDirection);
 
         // 1920x1080 images has 19.2 width (in PixelPerUnit 100)
-        if (transform.position.x < Camera.main.transform.position.x - 18.0f)
+        if (transform.position.x < Camera.main.transform.position.x - width * 0.99)
         {
-            transform.position = new Vector3(transform.position.x + 38.4f, transform.position.y, transform.position.z);
+            transform.position = new Vector3(transform.position.x + width * 2, transform.position.y, transform.position.z);
         }
 
-        if (transform.position.x > Camera.main.transform.position.x + 18.0f) 
+        if (transform.position.x > Camera.main.transform.position.x + width * 0.99) 
         {
-            transform.position = new Vector3(transform.position.x - 38.4f, transform.position.y, transform.position.z);
+            transform.position = new Vector3(transform.position.x - width * 2, transform.position.y, transform.position.z);
         }
     }
 }
