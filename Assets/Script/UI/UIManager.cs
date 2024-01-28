e1738bd1902246b38cc5a07c8b545342705bb6b3
2hys <tenjin62@gmail.com>
2024-01-28 17:40:25 +0900

    feat : 인게임 시간 구현 완료

--- Assets/Script/UI/UIManager.cs
+++ Assets/Script/UI/UIManager.cs
@@ -5,10 +5,11 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 public class UIManager : MonoBehaviour
 {
     [Header ("UI")]
-    [SerializeField] TextMeshProUGUI leftTime;
+    [SerializeField] TextMeshProUGUI setTimeTxt;
     [SerializeField] Text rightTime;
 
     [SerializeField] GameObject time;
@@ -19,17 +20,36 @@ public class UIManager : MonoBehaviour
     // Start is called before the first frame update
 
     private bool isPause = false;
+    public float setTime;
 
     void Start()
     {
-        
+        setTime = 540.0f;
     }
 
     // Update is called once per frame
 
     void Update()
     {
-        
+        Console.WriteLine(setTime);
+        if (setTime < 1260.0f)
+        {
+            setTime += Time.deltaTime;
+            setTime += Time.deltaTime;
+            setTime += Time.deltaTime;
+            setTime += Time.deltaTime;
+            setTime += Time.deltaTime;
+            setTime += Time.deltaTime;
+            setTime += Time.deltaTime;
+            setTime += Time.deltaTime;
+            setTime += Time.deltaTime;
+        }
+
+        else if (setTime > 1260.0f)
+            Time.timeScale = 0.0f;
+        setTimeTxt.text = Mathf.Floor(setTime / 60f).ToString().PadLeft(2, '0') + 
+            " : " + 
+            Mathf.Floor(setTime % 60f).ToString().PadLeft(2, '0');
         Console.WriteLine("onESC");
         if (Input.GetKeyDown(KeyCode.Escape))
         {
@@ -46,11 +66,32 @@ public class UIManager : MonoBehaviour
             }
             Time.fixedDeltaTime = 0.02f * Time.timeScale;
         }
-        
     }
 
     public void goaldistance()
     {
         
+    }
+
+    public void CountDown()
+    {
+
+    }
+
+    public void sceneChangeBtn(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void resumeBtn()
+    {
+        isPause = false;
+        Time.timeScale = 1.0f;
+        settings.SetActive(false);
+    }
+
+    public void getItem()
+    {
+
     }
 }
