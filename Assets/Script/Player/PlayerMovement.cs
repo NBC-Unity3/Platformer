5d91d5a867f9f6a1467d220921a18d36491fc0be
Eun6636 <tktmdmswl004@gmail.com>
2024-01-30 16:37:24 +0900

    fix: 캐릭터애니메이션

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -68,7 +68,7 @@ public class PlayerMovement : MonoBehaviour
             rigidbody.velocity = velocity;
             rigidbody.AddForce(direction * (jump - 2), ForceMode2D.Impulse);
 
-            animation.SecondJump();
+            animation.SecondJump(rigidbody);
         }
     }
 
@@ -95,11 +95,12 @@ public class PlayerMovement : MonoBehaviour
             audioManager.PlaySFX(audioManager.walkClip);
 
 
-        velocity.x = direction.x * speed; // x �ӵ� ����
+        velocity.x = direction.x * speed; 
         velocity.y = rigidbody.velocity.y;
         rigidbody.velocity = velocity;
 
-        //��������Ʈ ����
+
+
         if (velocity.x < 0)
         {
             sprite.flipX = true;
