4b946acf653df0e207c1d424284db6c56b72a527
STIKA <graysky9@naver.com>
2024-01-30 16:31:59 +0900

    clear 장애물화
    클리어태그만듬
    raincollision destory 아랫줄로
    level->clear->
    Clear trap으로 이동
    프리팹temp삭제
    computer삭제
    sprites->coupter삭제
    drop 조이스틱 닌텐도로 변경
    조이스틱삭제
    
    장애물 3가지여서 장애물별 태그만듬
    
    fix -> 침대, 닌텐도
    drop->닌텐도
    rain->youtube
    
    장애물별 텍스트 변경

--- Assets/Script/Obstacle/Obstacle.cs
+++ Assets/Script/Obstacle/Obstacle.cs
@@ -21,18 +21,40 @@ public class Obstacle : ModeChoice
 
             if (coll.gameObject.tag == "Player")
             {
-                uiManager.endResult("한효승 매니저님이 진실의 방으로 끌고갔다");
-                //TODO
-                Debug.Log("Player부딪힘");
+                if (this.gameObject.tag == "Clear")
+                {
+                    uiManager.endResult("Clear!");
+                }
+                if (this.gameObject.tag == "Bed")
+                {
+                    uiManager.endResult("폭신한 침대의 유혹에 지고 말았다.\n" +
+                        "시계를 보니 밤 12시가 지났다....\n" +
+                        "더 무서운건 퇴실버튼을 못 눌러서 매니저님이 찾아왔다..");
+                }
+                if (this.gameObject.tag == "Nintendo")
+                    uiManager.endResult("몰래 게임을 하다가 걸려서 한효승 매니저님이 진실의 방으로 끌고갔다");
+
+                Debug.Log($"{gameObject.tag} drop or fly랑 Player부딪힘");
             }
         }
         else
         {
             if (coll.gameObject.tag == "Player") //Fix인 Obstacle이랑 부딪혔을때
             {
-                uiManager.endResult("유튜브의 알고리즘이 나를 현혹시킨다.");
-                //TODO
-                Debug.Log($"{gameObject.name} elsePlayer부딪힘");
+                if(this.gameObject.tag == "Clear")
+                {
+                    uiManager.endResult("Clear!");
+                }
+                if(this.gameObject.tag == "Bed")
+                {
+                    uiManager.endResult("폭신한 침대의 유혹에 지고 말았다.\n" +
+                        "시계를 보니 밤 12시가 지났다....\n" +
+                        "더 무서운건 퇴실버튼을 못 눌러서 매니저님이 찾아왔다..");
+                }
+                if (this.gameObject.tag == "Nintendo")
+                    uiManager.endResult("몰래 게임을 하다가 걸려서 한효승 매니저님이 진실의 방으로 끌고갔다");
+                
+                Debug.Log($"{gameObject.tag} fix랑 Player부딪힘");
             }
         }
     }
